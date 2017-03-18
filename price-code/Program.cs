1ee1005c8c2144090a352fe50d6827e92f05ce12
travis85904 <travis85904@gmail.com>
2017-03-18 13:20:45 -0700

    initial

---
+++ price-code/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace price_code_discount_clean2
+{
+    internal class Program
+
+
+    {
+        static string priceCode;
+        static float iPrice;
+        static float priceCodeNum;
+
+        public static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Welcome to Pricer Pro v0.2!\nEnter pricing code as \"quit\" when done");
+                GetInput();
+                priceCodeNum = PriceCodeSwitch();
+                CalcPrice();
+            } while (priceCode != "QUIT");
+        }
+
+        static void GetInput()
+        {
+            Console.WriteLine("Enter pricing code: ");
+            priceCode = Console.ReadLine().ToUpper().;
+            Console.WriteLine("Enter item price: ");
+            iPrice = float.Parse(Console.ReadLine());
+        }
+        static float PriceCodeSwitch()
+        {
+            switch (priceCode)
+            {
+                case "H":
+                    return 0.5F;
+                case "F":
+                    return 0.4F;
+                case "T":
+                    return 0.33F;
+                case "Q":
+                    return 0.25F;
+                default:
+                    return 0;
+            }
+        }
+
+        private static void CalcPrice()
+        {
+            Console.WriteLine(("Original Price: $" + iPrice + "   Discount: $" + (iPrice * priceCodeNum) +
+                               "   New price: $" + (iPrice - (iPrice * priceCodeNum))));
+        }
+    }
+}
---
+++ simple-classes/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simple_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            car myNewCar = new car();
+
+            myNewCar.make = "Ford";
+            myNewCar.model = "Expedition";
+            myNewCar.year = 2004;
+            myNewCar.color = "Blue";
+
+            Console.WriteLine("{0} - {1} - {2}",
+            myNewCar.make,
+            myNewCar.model,
+            myNewCar.color);
+
+            //double marketValueOfCar = determineMarketValue(myNewCar);
+            Console.WriteLine("Car's value: {0:C}", myNewCar.DetermineMarketValue()); // {0:C} return value with a $ symbol
+            Console.ReadLine();
+
+        
+        }
+
+        private static double determineMarketValue(car car)
+        {
+            double carValue = 100.0;
+
+            // may go online some day to lookup car's value
+            // and retrieve its value into carValue variable
+            return carValue;
+        }
+
+    }
+
+    class car
+    {
+        public string make { get; set; }
+        public string model { get; set; }
+        public int year { get; set; }
+        public string color { get; set; }
+
+        public double DetermineMarketValue()
+        {
+            double carValue = 100.0;
+
+            if (this.year > 1990)
+                carValue = 10000.0;
+            else
+                carValue = 2000.0;
+
+            return carValue;
+        }
+    }
+
+
+
+}
